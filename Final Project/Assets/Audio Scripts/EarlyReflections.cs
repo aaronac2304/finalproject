1e13162d19359c4ee455688d11ad372d301f9ebd
aaronac2304 <aaguayocalderon@berklee.edu>
2018-04-18 21:16:20 -0400

    Scripts

---
+++ Final Project/Assets/Audio Scripts/EarlyReflections.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarlyReflections : MonoBehaviour
+{
+    [Range(0f, 500f)]
+    public float rayDist = 50f;
+
+    void Update()
+    {
+
+    }
+
+    private void occlusionRaycast()
+    {
+        RaycastHit objectHit1;
+        RaycastHit objectHit2;
+        RaycastHit objectHit3;
+        RaycastHit objectHit4;
+        RaycastHit objectHit5;
+        RaycastHit objectHit6;
+
+
+        Vector3 fwd = this.transform.TransformDirection(Vector3.forward);
+        if (Physics.Raycast(this.transform.position, fwd, out objectHit1, rayDist))
+        {
+
+        }
+
+        Vector3 bck = this.transform.TransformDirection(Vector3.back);
+        if (Physics.Raycast(this.transform.position, bck, out objectHit2, rayDist))
+        {
+
+        }
+
+        Vector3 lft = this.transform.TransformDirection(Vector3.left);
+        if (Physics.Raycast(this.transform.position, lft, out objectHit3, rayDist))
+        {
+
+        }
+
+        Vector3 rgt = this.transform.TransformDirection(Vector3.right);
+        if (Physics.Raycast(this.transform.position, rgt, out objectHit4, rayDist))
+        {
+
+        }
+
+        Vector3 dwn = this.transform.TransformDirection(Vector3.down);
+        if (Physics.Raycast(this.transform.position, fwd, out objectHit5, rayDist))
+        {
+
+        }
+
+        Vector3 up = this.transform.TransformDirection(Vector3.up);
+        if (Physics.Raycast(this.transform.position, fwd, out objectHit6, rayDist))
+        {
+
+        }
+    }
+}
---
+++ Final Project/Assets/Audio Scripts/Occlusion.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(AudioLowPassFilter))]
+
+public class Occlusion : MonoBehaviour
+{
+    AudioSource audioSource;
+
+    private Vector3 sourcePos;
+    private Vector3 playerPos;
+    private Vector3 rayDirection;
+    private Vector3 fwd = Vector3.forward;
+
+    float angle;
+
+
+    private bool obst;
+    public bool isDirectional = false;
+
+
+
+    public GameObject player;
+
+    private bool isObstructed;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+
+        sourcePos = transform.position;
+        playerPos = player.transform.position;
+        rayDirection = playerPos - sourcePos;
+
+        //raycastOcclusion();
+
+        if (isDirectional == true)
+        {
+            audioSource.volume = directionalSound();
+        }
+
+        Debug.Log(angle);
+    }
+
+    private float directionalSound()
+    {
+        angle = Vector3.Angle(fwd, rayDirection);
+        float vol = angle / 180f;
+        return vol;
+
+    }
+
+    void raycastOcclusion()
+    {
+        float dist = Vector3.Distance(sourcePos, playerPos);
+        Debug.DrawRay(sourcePos, rayDirection, Color.green);
+        dist = Vector3.Distance(playerPos, sourcePos);
+
+        RaycastHit hit;
+
+        bool obst = Physics.Raycast(sourcePos, rayDirection, out hit, dist);
+
+        if (obst == false)
+        {
+            GetComponent<AudioLowPassFilter>().cutoffFrequency = 22000;
+        }
+        else { GetComponent<AudioLowPassFilter>().cutoffFrequency = 2000; }
+    }
+}
